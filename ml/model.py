3b16ffe114adb4881190fad1928915b83efb96ff
Slimseidl <john_seidl@outlook.com>
2025-06-06 13:52:45 -0500

    Updated train_model function

--- ml/model.py
+++ ml/model.py
@@ -1,6 +1,8 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 # TODO: add necessary import
 
 # Optional: implement hyperparameter tuning.
@@ -20,7 +22,9 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
